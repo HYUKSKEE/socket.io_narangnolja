de05df3fa1d931815f8304ece6275e3105b166d5
HYUKSKEE <gin280833@gmail.com>
2023-01-03 15:32:35 +0900

    server dat 수신 작업중

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,20 @@
-const a = document.querySelector(".a");
-a.innerHTML = "반가워";
-console.log(a);
+import { io } from "socket.io-client";
+
+const h = document.querySelector(".a");
+const socket = io("ws://localhost:3001");
+const name = "김진혁";
+const msg = "뭐하냐 나와";
+
+socket.on("connect", () => {
+  // ...
+  console.log("connect");
+});
+
+socket.emit(name, `${name} : ${msg}`, (response) => {
+  console.log(response); // "got it"
+  h.append(
+    "<div>" + response + " : from <strong>" + response + "</strong></div>"
+  );
+
+  //socket.on(name, function (data) {});
+});
